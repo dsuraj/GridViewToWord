1f5da4ed61b60aba8bd07f8d76666a9be373ce39
dsuraj <deshpande.suraj@gmail.com>
2013-09-01 21:29:47 +0530

    Initial Commit

---
+++ GridViewToWord/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridViewToWord
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=EmployeesData.doc");
+            Response.ContentType = "application/ms-word";
+
+            StringWriter stringWriter = new StringWriter();
+            HtmlTextWriter htmlTextWriter = new HtmlTextWriter(stringWriter);
+
+            GridView1.AllowPaging = false;
+            GridView1.DataBind();
+            GridView1.RenderControl(htmlTextWriter);
+
+            Response.Write(stringWriter.ToString());
+            Response.End();
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Verifies that the control is rendered */
+        }
+    }
+}
